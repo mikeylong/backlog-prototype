cceff8c1273303aa3115a78275a29e286bedd0b4
tworker <tworker@thoughtworks.com>
2012-08-14 13:31:05 -0700

    Relationship.

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,5 +1,5 @@
 class Objective < ActiveRecord::Base
   belongs_to :plan
   belongs_to :program
-  attr_accessible :title
+  attr_accessible :title, :program_id, :plan_id
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -2,9 +2,9 @@ class Program < ActiveRecord::Base
   attr_accessible :title
   
   has_one :plan
-  has_many :projects, through: :plans
+  has_many :projects, through: :plan
   has_many :objectives
-  
+
   def backlog
     @objectives
   end
