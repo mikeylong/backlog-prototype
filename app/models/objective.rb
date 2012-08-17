9e1b0dfbf22e8e612437dd3deaf4073c9071b800
tworker <tworker@thoughtworks.com>
2012-08-17 11:29:25 -0700

    ensuring peeps can't break the proto

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -2,6 +2,11 @@ class Objective < ActiveRecord::Base
   belongs_to :plan
   belongs_to :program
   attr_accessible :title, :cost, :value, :program_id, :plan_id, :position
+  validates :title, presence: :true
+  validates :cost, presence: :true
+  validates :value, presence: :true
+  validates_numericality_of :cost, :only_integer => true, :message => "Can only be whole number."
+  validates_numericality_of :value, :only_integer => true, :message => "Can only be whole number."
   acts_as_list scope: :program
   def ratio
     (value * 100 / cost)
