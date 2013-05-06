08b40659e60fe65566d238350a01f9b658d3eb61
Mike Long <mblongii@gmail.com>
2013-05-06 15:25:20 -0700

    fixing tests

--- test/functional/objectives_controller_test.rb
+++ test/functional/objectives_controller_test.rb
@@ -3,45 +3,48 @@ require 'test_helper'
 class ObjectivesControllerTest < ActionController::TestCase
   setup do
     @objective = objectives(:one)
+    @program = programs(:one)
+    @objective.program = @program
+    @objective.save
   end
 
   test "should get index" do
-    get :index
+    get :index, program_id: @program
     assert_response :success
     assert_not_nil assigns(:objectives)
   end
 
   test "should get new" do
-    get :new
+    get :new, program_id: @program
     assert_response :success
   end
 
   test "should create objective" do
     assert_difference('Objective.count') do
-      post :create, objective: { title: @objective.title }
+      post :create, objective: { title: @objective.title }, program_id: @program
     end
 
     assert_redirected_to objective_path(assigns(:objective))
   end
 
   test "should show objective" do
-    get :show, id: @objective
+    get :show, id: @objective, program_id: @program
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @objective
+    get :edit, id: @objective, program_id: @program
     assert_response :success
   end
 
   test "should update objective" do
-    put :update, id: @objective, objective: { title: @objective.title }
+    put :update, id: @objective, objective: { title: @objective.title }, program_id: @program
     assert_redirected_to objective_path(assigns(:objective))
   end
 
   test "should destroy objective" do
     assert_difference('Objective.count', -1) do
-      delete :destroy, id: @objective
+      delete :destroy, id: @objective, program_id: @program
     end
 
     assert_redirected_to objectives_path
--- test/unit/helpers/backlogs_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BacklogsHelperTest < ActionView::TestCase
-end
